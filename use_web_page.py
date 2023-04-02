007e223f0ee13e8f4751ac08349241e1df02fb98
Anca <anca.constantin1@gmail.com>
2023-04-02 18:44:23 +0100

    Find the tag containing the price

--- use_web_page.py
+++ use_web_page.py
@@ -4,8 +4,28 @@ from bs4 import BeautifulSoup
 import requests
 
 # send an http request to this url, and return the content of the page
-url = 'https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product'
+url = 'https://www.newegg.ca/asus-geforce-rtx-4070-ti-tuf-rtx4070ti-o12g-gaming/p/N82E16814126606'
 my_result = requests.get(url)
 
 # store the content of the page in my_result.text
-print(my_result.text)
+# print(my_result.text)
+
+# use BeautifulSoup 
+my_document = BeautifulSoup(my_result.text, 'html.parser')
+# print the html text, with tags and all
+# print(my_document.prettify())
+
+# find the price in the html page
+# 1. find '$'
+prices = my_document.find_all(text='$')
+print(prices)
+
+# find the parent of '$'
+parent = prices[0].parent
+print(parent)
+
+# now search for the parent tag where we see the price
+parent_tag = parent.find('strong')
+print(parent_tag)
+#and to only get the actual number representing the price:
+print(parent_tag.string)
